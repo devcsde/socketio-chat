033bc99391db88c844cc9c6a17ebd9c536916bea
devcsde <c.scheidler@gmx.net>
2017-08-08 23:27:14 +0200

    test

---
+++ dist/server/server.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * Created by csche on 03.08.2017.
+ */
+
+var path = require("path");
+var http = require("http");
+var express = require("express");
+var socketIO = require("socket.io");
+
+var publicPath = path.join(__dirname, "../public");
+var port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+app.use(express.static(publicPath));
+
+io.on("connection", function (socket) {
+    console.log("New user connected");
+
+    socket.on("disconnect", function () {
+        console.log("User was disconnected");
+    });
+});
+
+server.listen(port, function () {
+    console.log("Started up at port " + port);
+});
+//# sourceMappingURL=server.js.map
--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 /**
  * Created by csche on 03.08.2017.
  */
+
 const path = require("path");
+const http = require("http");
 const express = require("express");
+const socketIO = require("socket.io");
 
 const publicPath =  path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
-
 let app = express();
+let server = http.createServer(app);
+let io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    socket.on("disconnect", () => {
+        console.log("User was disconnected");
+    });
+});
+
+
+server.listen(port, () => {
     console.log(`Started up at port ${port}`);
 });
